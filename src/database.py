b7de694b0466cae86c092a6365a6e2af859bd2c6
savio37 <savio.am73@.com>
2024-08-22 08:59:29 -0300

    query list filter

--- src/database.py
+++ src/database.py
@@ -1,5 +1,4 @@
 from tinydb import TinyDB, Query
-from tinydb.table import Table
 
 db = TinyDB('data/database.json')
 pokemon_table = db.table('pokemons')
@@ -16,20 +15,32 @@ class FacadeDB:
     def add_species(self, species:dict):
         self.table_species.insert(species)
         
-    def get_species(self, key:str, value):
-        return self.table_species.search(self.entry[key] == value)
+    def get_species(self, value:int|str|list[int]|list[str], key:str='id'):
+        if isinstance(value, list):
+            return self.table_species.search(self.entry[key] in value)
+        else:
+            return self.table_species.search(self.entry[key] == value)
     
-    def del_species(self, key:str, value):
-        self.table_species.remove(self.entry[key] == value)
+    def del_species(self, value:int|str|list[int]|list[str], key:str='id'):
+        if isinstance(value, list):
+            self.table_species.remove(self.entry[key] in value)
+        else:
+            self.table_species.remove(self.entry[key] == value)
         
     def add_type(self, type:dict):
         self.table_types.insert(type)
         
-    def get_type(self, key:str, value):
-        return self.table_types.search(self.entry[key] == value)
+    def get_type(self, value:int|str|list[int]|list[str], key:str='id'):
+        if isinstance(value, list):
+            return self.table_types.search(self.entry[key] in value)
+        else:
+            return self.table_types.search(self.entry[key] == value)
     
-    def del_type(self, key:str, value):
-        self.table_types.remove(self.entry[key] == value)
+    def del_type(self, value:int|str|list[int]|list[str], key:str='id'):
+        if isinstance(value, list):
+            self.table_types.remove(self.entry[key] in value)
+        else:
+            self.table_types.remove(self.entry[key] == value)
         
     def clear_species(self):
         self.table_species.truncate()
