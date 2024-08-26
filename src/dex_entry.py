e97e7a48049f34a0d3db120919e06d5aaf91ca3e
savio37 <savio.am73@.com>
2024-08-26 06:58:50 -0300

    dex popup

--- src/dex_entry.py
+++ src/dex_entry.py
@@ -89,6 +89,64 @@ class AppFormEntry(QFrame):
         self.layout_frame = QGridLayout()
         self.layout_frame.setContentsMargins(15, 15, 15, 15)
         self.layout_frame.setSpacing(0)
+        self.setLayout(self.layout_frame)
+    
+        self.pokemon = None
+        
+        self.label_num = QLabel()
+        self.label_num.setStyleSheet(f"""font-size: {DefaultFont.SIZE+6}pt;""")
+        self.layout_frame.addWidget(self.label_num, 0, 0, 1, 1, Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        
+        self.label_especie = QLabel()
+        self.label_especie.setStyleSheet(f"""font-size: {DefaultFont.SIZE+6}pt;""")
+        self.layout_frame.addWidget(self.label_especie, 0, 1, 1, 2, Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        
+        self.img_especie = AppImage()
+        self.img_especie.setFixedSize(256, 256)
+        self.layout_frame.addWidget(self.img_especie, 1, 0, 4, 2, Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        
+        self.img_tipo_1 = AppImage()
+        self.img_tipo_1.setFixedSize(152, 36)
+        
+        self.img_tipo_2 = AppImage()
+        self.img_tipo_2.setFixedSize(152, 36)
+        
+        label = QLabel("Height")
+        label.setStyleSheet(f"""font-size: {DefaultFont.SIZE+4}pt;""")
+        self.label_height = QLabel()
+        self.label_height.setStyleSheet(f"""font-size: {DefaultFont.SIZE+4}pt; font-weight: normal;""")
+        self.layout_frame.addWidget(label, 2, 2, 1, 1, Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignBottom)
+        self.layout_frame.addWidget(self.label_height, 3, 2, 1, 1, Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        
+        label = QLabel("Weight")
+        label.setStyleSheet(f"""font-size: {DefaultFont.SIZE+4}pt;""")
+        self.label_weight = QLabel()
+        self.label_weight.setStyleSheet(f"""font-size: {DefaultFont.SIZE+4}pt; font-weight: normal;""")
+        self.layout_frame.addWidget(label, 2, 3, 1, 1, Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignBottom)
+        self.layout_frame.addWidget(self.label_weight, 3, 3, 1, 1, Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        
+        self.label_desc = QLabel()
+        self.label_desc.setStyleSheet(f"""font-size: {DefaultFont.SIZE+2}pt; font-weight: normal;""")
+        self.label_desc.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+        self.label_desc.setWordWrap(True)
+        self.label_desc.setAlignment(Qt.AlignmentFlag.AlignJustify)
+        self.layout_frame.addWidget(self.label_desc, 4, 2, 1, 2, Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        
         
     def set_species(self, species: dict):
-        pass
+        self.pokemon = species
+        self.label_num.setText(f"#{species['id']:03}")
+        self.label_especie.setText(species['name'])
+        self.img_especie.setImage(species['img'])
+        self.img_tipo_1.setImage(species['types'][0]['img'])
+        if len(species['types']) > 1:
+            self.img_tipo_2.setImage(species['types'][1]['img'])
+            self.layout_frame.addWidget(self.img_tipo_1, 1, 2, 1, 1, Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
+            self.layout_frame.addWidget(self.img_tipo_2, 1, 3, 1, 1, Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        else:
+            self.layout_frame.addWidget(self.img_tipo_1, 1, 2, 1, 2, Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+            
+        self.label_height.setText(f"{species['height']} m")
+        self.label_weight.setText(f"{species['weight']} kg")
+        self.label_desc.setText(species['description'])
+        
